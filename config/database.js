70df5010a85ab3838e22e01a4c62e72a73b26048
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-10-02 14:40:47 +0000

    env file added

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://Honeshwar:cAs8Q7hDGiFzijcU@cluster0.o5zojlu.mongodb.net/?retryWrites=true&w=majority",
-); //.then(() => {
+mongoose.connect(process.env.MONGODB_STRING_URL); //.then(() => {
 //     console.log("Successfully connecting with mongoodb")
 // }).catch(() => {
 //     console.log("Error in connecting with mongoodb")
