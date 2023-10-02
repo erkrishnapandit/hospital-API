c90f3099cbf4cbb606408125e52dc423765bea29
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-10-02 14:28:08 +0000

    reslove jwt token issue

--- config/database.js
+++ config/database.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/HOSPITAL_API'); //.then(() => {
+mongoose.connect(
+  "mongodb+srv://Honeshwar:cAs8Q7hDGiFzijcU@cluster0.o5zojlu.mongodb.net/?retryWrites=true&w=majority",
+); //.then(() => {
 //     console.log("Successfully connecting with mongoodb")
 // }).catch(() => {
 //     console.log("Error in connecting with mongoodb")
 // });
 
-
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'error in connecting with mongoodb'));
-db.once('open', ()=>{
-    console.log("Successfully connecting with mongoodb");
+db.on(
+  "error",
+  console.error.bind(console, "error in connecting with mongoodb"),
+);
+db.once("open", () => {
+  console.log("Successfully connecting with mongoodb");
 });
 
 module.exports = db;
